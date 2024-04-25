44cb38275c0d6f93074bd1da1f6b3e8bd0a3cfdc
Trevor Skillman <trevor.skillman@snhu.edu>
2024-04-25 19:29:55 -0400

    Working Layout

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('/starters') # Dynamic route
+def starters():
+    # We need to get and display the items inside of starters
+    starters = get_starters()
+    return render_template('starters.html', starters=starters)
 
